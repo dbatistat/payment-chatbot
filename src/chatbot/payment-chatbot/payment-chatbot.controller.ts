b290ce50701ebd0cd41dcc738a545a0e3c09f06d
David Batista Taboada <david.batista@swissbytes.ch>
2019-06-29 12:40:25 -0400

    Fixed webhook test

--- src/chatbot/payment-chatbot/payment-chatbot.controller.ts
+++ src/chatbot/payment-chatbot/payment-chatbot.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, HttpCode, Post } from '@nestjs/common';
 import { Body } from '@nestjs/common/decorators/http/route-params.decorator';
 
 @Controller('payment_chatbot')
 export class PaymentChatbotController {
   @Post()
+  @HttpCode(200)
   getHello(@Body() data: any): string {
     return 'EVENT_RECEIVED';
   }
