d19a12789556ddae55567e117c08e17c57b09818
David Batista Taboada <david.batista@swissbytes.ch>
2019-06-29 12:55:46 -0400

    Fixed webhook test

--- src/chatbot/payment-chatbot/payment-chatbot.controller.ts
+++ src/chatbot/payment-chatbot/payment-chatbot.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, HttpCode, Post } from '@nestjs/common';
 import { Body } from '@nestjs/common/decorators/http/route-params.decorator';
 
-@Controller('payment_chatbot')
+@Controller('webhook')
 export class PaymentChatbotController {
   @Post()
   @HttpCode(200)
